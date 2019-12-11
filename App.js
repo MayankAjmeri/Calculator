e5567c89bd3b91b1383650ac44b1503a65efd319
MayankAjmeri <mayankajmeri@gmail.com>
2019-12-11 15:46:05 +0530

    Added buttons and text

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import {
 	StyleSheet,
-	View
+	View,
+	Button,
+	Text,
 } from 'react-native';
 
 export default class App extends Component {
@@ -14,11 +16,46 @@ export default class App extends Component {
 	render() {
 		return (
 			<View style={styles.container}>
-				<View style={styles.calculation}></View>
-				<View style={styles.result}></View>
+				<View style={styles.calculation}>
+					<Text style={styles.calculationText}>
+						11*11
+					</Text>
+				</View>
+				<View style={styles.result}>
+					<Text style={styles.resultText}>
+						121
+				</Text>
+				</View>
 				<View style={styles.buttons}>
-					<View style={styles.numbers}></View>
-					<View style={styles.operations}></View>
+					<View style={styles.numbers}>
+						<View style={styles.row}>
+							<Button title="7" />
+							<Button title="8" />
+							<Button title="9" />
+						</View>
+						<View style={styles.row}>
+							<Button title="4" />
+							<Button title="5" />
+							<Button title="6" />
+						</View>
+						<View style={styles.row}>
+							<Button title="1" />
+							<Button title="2" />
+							<Button title="3" />
+						</View>
+						<View style={styles.row}>
+							<Button title="." />
+							<Button title="0" />
+							<Button title="0" />
+						</View>
+					</View>
+					<View style={styles.operations}>
+						<Button title="DEL" />
+						<Button title="/" />
+						<Button title="*" />
+						<Button title="-" />
+						<Button title="+" />
+					</View>
 				</View>
 			</View>
 		);
@@ -31,11 +68,27 @@ const styles = StyleSheet.create({
 	},
 	calculation: {
 		flex: 1.8,
-		backgroundColor: 'white'
+		backgroundColor: 'white',
+		justifyContent: "center",
+		alignItems: "flex-end",
+	},
+	calculationText: {
+		fontSize: 40,
+		textAlignVertical: "center",
+		padding: 15,
+		// alignSelf: "flex-end",
 	},
 	result: {
 		flex: 1.2,
-		backgroundColor: 'green'
+		backgroundColor: 'green',
+		justifyContent: "center",
+		alignItems: "flex-end",
+	},
+	resultText: {
+		fontSize: 30,
+		textAlignVertical: "center",
+		alignSelf: "flex-end",
+		padding: 15,
 	},
 	buttons: {
 		flex: 6,
@@ -44,8 +97,12 @@ const styles = StyleSheet.create({
 	numbers: {
 		flex: 3,
 		backgroundColor: '#434a3e',
+	},
+	row: {
+		flex: 1,
+		flexDirection: "row",
 		alignItems: "center",
-		justifyContent: 'space-evenly'
+		justifyContent: "space-around"
 	},
 	operations: {
 		flex: 1,
